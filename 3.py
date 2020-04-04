be95d48f34a7ad9287362a1ea776fd1b650b5c3b
Chawsukhin <63122620+Chawsukhin@users.noreply.github.com>
2020-04-04 07:06:44 -0700

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,12 @@
+class House:
+ def __init__(self, brick, wood):
+     self.brick = brick
+     self.wood=wood
+h1=House(30000,1000)
+print(h1.brick)
+print(h1.wood)
+
+class abc:
+ x=5
+o=abc()
+print(o.x)
